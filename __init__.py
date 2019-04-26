f43ba36cc6e4d2f18605b83a6648d9b979062b88
AIginger <gingerxjiang@gmail.com>
2019-04-26 22:30:59 +0800

    the init file of package wix

---
+++ __init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 19 18:37:05 2019
+
+@author: HaiyanJiang
+@email : jianghaiyan.cn@gmail.com
+
+"""
+
+from wix.preview import rename_docfile, get_colnames
+from wix.xtable import WIXtable
+from wix.xclean import CleanXdata
+from wix.xydata import ConstructXYdata
+
+
+print(__doc__)
+
